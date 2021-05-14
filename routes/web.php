6e28aba656697feb7c9ea662565c7f5e7f727700
kazuma <cazuma11@yahoo.co.jp>
2021-05-14 10:13:12 +0000

    Laravel 12課題

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,11 @@ Route::group(['prefix' => 'admin'], function() {
 });
 Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create',
-'Admin\ProfileController@add');
+/* Lalavel12 課題２*/
+'Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit',
-'Admin\ProfileController@edit');
+/*Laravel12 課題３*/
+'Admin\ProfileController@edit')->middleware('auth');
 });
 
 Auth::routes();
