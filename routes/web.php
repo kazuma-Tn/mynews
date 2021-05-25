676831bc988ab575789fb81452ef4255c572c1a7
kazuma <cazuma11@yahoo.co.jp>
2021-05-25 11:59:31 +0000

    Laravel 17

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-     Route::post('news/create', 'Admin\NewsController@create')->middleware('auth'); 
-     Route::get('news', 'Admin\NewsController@index')->middleware('auth');
-     Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
-     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
-     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create'); 
+     Route::get('news', 'Admin\NewsController@index');
+     Route::get('news/edit', 'Admin\NewsController@edit');
+     Route::post('news/edit', 'Admin\NewsController@update');
+     Route::get('news/delete', 'Admin\NewsController@delete');
      //Laravel13 課題３
      Route::get('profile/create', 'Admin\ProfileController@add');
      Route::post('profile/create', 'Admin\ProfileController@create');
